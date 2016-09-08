5880064ca196096481198d9b261131cd67c68b8a
Lacy Thomas <lacy.thomas@weatheranalytics.com>
2016-09-08 08:41:47 -0400

    switching localhost and heroku

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ var mongodb = require("mongodb");
 var client = require('mongodb').MongoClient;
 var mongoUri = process.env.MONGOLAB_URI ||
   process.env.MONGOHQ_URL 
-  || 'mongodb://localhost:27017/hiking' ||
-  'mongodb://lthomas:password@ds023054.mlab.com:23054/heroku_fb2fccgv';
+  || 'mongodb://lthomas:password@ds023054.mlab.com:23054/heroku_fb2fccgv'
+  || 'mongodb://localhost:27017/hiking';
  var db;
 
 // configure app to use bodyParser()
